5551870caeb582710040fe99e230ba14d402e103
princemclein <princemcleinjgonzales@gmail.com>
2026-08-14 16:43:18 +0800

    Fix the filtering button.

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,29 @@
-document.querySelectorAll();
+const filterButton = document.querySelectorAll(".filter-btn");
+const products = document.querySelectorAll(".menu-card");
+
+// Functions for filtering;
+filterButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const filter = button.dataset.filter;
+
+    products.forEach((product) => {
+      const category = product.dataset.category;
+      if (filter === "all" || category === filter) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
+      }
+    });
+  });
+});
+
+// Hover on active filter button;
+filterButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    filterButton.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+  });
+});
